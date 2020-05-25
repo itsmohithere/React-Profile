9a9950b4744f9214468a2561c91b27add48362c9
itsmohithere <itsmohitsaxenahere@gmail.com>
2020-05-25 18:37:33 +0530

    phase-1: title update

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -5,7 +5,7 @@ import Card from '../../commonComponents/Card/Card';
 
 export default function About() {
   const boldLetterPOne = <strong>CMS, CRM, Admin Panel and websites.</strong>;
-  const pOneText = 'Hello! I am passionate JavaScript developer and have handfull of expirience in developing ';
+  const pOneText = 'Hello! I am passionate JavaScript developer and have handfull of experience in developing ';
   const pTwoText = 'I define strategies & communications that you will need to transform your business experiences and improve people’s lives. By digging into business problems, I analyse, define and solve them with the help of productive ideas.';
   return (
     <Card className="cardContainer">
