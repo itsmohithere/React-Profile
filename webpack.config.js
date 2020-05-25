faabe535f3043171f525c9e2cfc0642057b8765e
itsmohithere <itsmohitsaxenahere@gmail.com>
2020-05-25 17:19:59 +0530

    phase-1: update webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
+    publicPath: '',
     filename: 'bundle.js',
   },
   devServer: {
